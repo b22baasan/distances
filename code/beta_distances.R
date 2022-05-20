4219643215986f37e6d02a6e7eaf09482468a5e4
b22baasan <b22baasan@yahoo.com>
2022-05-20 15:38:30 -0400

    first graphic

---
+++ code/beta_distances.R
@@ -0,0 +1,64 @@
+# author: bbaasan
+# Date:
+# version:
+# Purpose: showing alternatives to ordination for displaying beta-diversity data with tidyverse in R
+
+library(tidyverse)
+library(vegan)
+
+# raw_data location
+list.files('data')
+
+days_wanted <- c(0:9, 141:150)
+
+shared_tbl <- read_tsv('data/mice.shared') %>% 
+        mutate(days = as.numeric(str_replace(Group, ".*D", ""))) %>% 
+        filter(days %in% days_wanted) %>% 
+        select(Group, starts_with("Otu"))
+
+shared_tbl %>% 
+        pivot_longer(-Group) %>% 
+        group_by(Group) %>% 
+        summarise(n_seqs = sum(value)) %>% 
+        arrange(n_seqs) %>% 
+        print(n=15)
+
+shared_df <- shared_tbl %>% 
+        column_to_rownames('Group') # makes col to row names
+
+# generate distance matrix - bray-curtis/rarefied to 18XX
+mice_dist <- avgdist(shared_df, sample = 1828)
+
+mice_dist %>% 
+        as.matrix() %>% 
+        as_tibble(rownames = 'samples') %>% 
+        pivot_longer(-samples) %>% 
+        filter(samples < name) %>% 
+        mutate(animal_a = str_replace(samples, "D.*", ""), 
+               animal_b = str_replace(name, "D.*", ""), 
+               day_a = str_replace(samples, ".*D", "") %>% as.double(),
+               day_b = str_replace(name, ".*D", "") %>% as.double(),
+               diff = abs(day_a - day_b),
+               day = if_else(day_b > day_a, day_b, day_a),
+               period = if_else(day < 10, "early", "late"),
+               sex = if_else(str_detect(animal_a, "F"), "female", "male")) %>% 
+        filter(animal_a == animal_b & diff == 1) %>% 
+        ggplot(aes(x=day, y=value, group=animal_a, color=sex))+
+        geom_line(size=1)+
+        facet_wrap(~period, scales='free_x', nrow=1)+
+        geom_smooth(aes(group=period), se=FALSE, color='black', size=2)+
+        scale_x_continuous(breaks=1:150)+
+        scale_color_manual(name=NULL,
+                           breaks = c("female", "male"),
+                           labels = c("Female", "Male"),
+                           values = c("purple", "limegreen"))+
+        labs(x="Days after weaning", 
+             y="Bray-Curtis distance to previous day")+
+        theme_classic()+
+        theme(
+                strip.text = element_blank(),
+                legend.position = c(0.8, 0.9)
+        )
+ggsave("figures/one_day_lag_plot.png")
+
+list.files('figures')
---
+++ data/README.md
@@ -0,0 +1,3 @@
+Alternatives to ordination with R: Displaying one day time lag in beta diversity (CC205)
+data source is here: https://github.com/riffomonas/distances/tree/43d6c6a63f07b0b118dec756987aab2f7be9c7ac
+video source: https://www.youtube.com/watch?v=roCRaWI3fmw
